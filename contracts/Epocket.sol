e6da365c8da99dec000e381c6336b7cbda1ccf5d
Muhindo-Galien <muhindogalien512@gmail.com>
2023-03-20 13:38:11 +0300

    upgated smart contract

--- contracts/Epocket.sol
+++ contracts/Epocket.sol
@@ -6,13 +6,10 @@ error Epocket__NotOwner();
 
 contract Epocket{
 
-    address private immutable i_owner;
-
     struct TransactionStruct{
         address from;
         address reciever;
         uint timestamp ;
-        string name;
         uint amount;
     }
 
@@ -21,6 +18,11 @@ contract Epocket{
     mapping(address => uint256) public transactionCount;
 
     TransactionStruct[] public transactions;
+
+    address private immutable i_owner;
+
+    event transctions(address indexed from, address to, uint amount, string symbol);
+    event recipeintTx(address indexed recipientOf,address recipient, string recipientName );
 
     // Modifiers
     modifier onlyOwner() {
@@ -32,31 +34,45 @@ contract Epocket{
         i_owner = msg.sender;
     }
 
-    function transferFund(address payable _reciever,string memory _name)external payable{
+    function _transfer(address payable _to, string memory symbol) external payable{
         require(msg.value > 0,"You can't send 0 ETH");
        
         uint serviceFee = ( msg.value * 5)/100;
         payTo(i_owner, serviceFee);
-        payTo(_reciever,(msg.value- serviceFee));
+        payTo(_to,(msg.value - serviceFee));
 
         TransactionStruct memory singleTransaction =TransactionStruct(
-            msg.sender,payable(_reciever),block.timestamp,_name,msg.value
+            msg.sender,payable(_to),block.timestamp,msg.value
         );
         // Add transaction for user
         transactionCount[msg.sender]++; // <-- transacion ID
         myTransactions[msg.sender][transactionCount[msg.sender]] = singleTransaction;
-        // Add singleTransaction to the array of all cnotract transactions
+        // Add singleTransaction to the array of all contract txs
         transactions.push(singleTransaction);
+
+        emit transctions( msg.sender,_to, msg.value, symbol);
     }
+
+    function saveTX(address from, address to, uint amount,string memory symbol) public {
+       emit transctions(from,to,amount, symbol);
+    }
+    
+    function addRecipeint(address recipient, string memory name) public {
+        emit recipeintTx(msg.sender, recipient, name);
+    }
+
     function payTo(address to, uint256 amount) internal {
         (bool success, ) = payable(to).call{value: amount}("");
-        require(success);
+        if(! success){
+           revert Epocket__TransferFailed();
+       }
     }
 
     function getBalanceAccount() public view returns (uint256){
         return msg.sender.balance;
     }
-     function getAllContracTxs() public view returns(TransactionStruct[] memory){
+
+    function getAllContracTxs() public view returns(TransactionStruct[] memory){
         return transactions;
     }
 
